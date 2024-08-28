eb51b9f9f43a79d414d325ab59ebc7abd7387844
harpreetsindhia999@gmail.com <harpreetsindhia999@gmail.com>
2024-08-28 20:23:47 +0530

    Commit from Git-With-Harry Architect

---
+++ src/test/java/TestngDemo/ComponentsTest.java
@@ -0,0 +1,13 @@
+package TestngDemo;
+
+import org.testng.annotations.Test;
+
+public class ComponentsTest {
+	
+	@Test
+	public void Framework()
+	{
+		System.out.println("Design pattern used");
+	}
+
+}
