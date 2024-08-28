4c3c4b3bc04c071e7df135b7d5f801254df2a597
harpreetsindhia999@gmail.com <harpreetsindhia999@gmail.com>
2024-08-28 19:53:18 +0530

    Another commit by Harpreet_Git

---
+++ src/test/java/TestngDemo/AppiumTest.java
@@ -0,0 +1,13 @@
+package TestngDemo;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void codeUpdate()
+	{
+		System.out.println("Learn Rest Assured Testing");
+	}
+
+}
