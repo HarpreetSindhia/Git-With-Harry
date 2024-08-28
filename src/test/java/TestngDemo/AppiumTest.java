163b2f112e98a958b4604142f846502c863f900a
harpreetsindhia999@gmail.com <harpreetsindhia999@gmail.com>
2024-08-28 19:58:59 +0530

    Another commit from Git-With-Harry

--- src/test/java/TestngDemo/AppiumTest.java
+++ src/test/java/TestngDemo/AppiumTest.java
@@ -9,5 +9,13 @@ public class AppiumTest {
 	{
 		System.out.println("Learn Rest Assured Testing");
 	}
+	
+	
+	
+	@Test
+	public void APITest()
+	{
+		System.out.println("Learing Java is mandatory for Selenium and Rest Assured");
+	}
 
 }
---
+++ src/test/java/TestngDemo/SecurityTest.java
@@ -0,0 +1,13 @@
+package TestngDemo;
+
+import org.testng.annotations.Test;
+
+public class SecurityTest {
+	
+	@Test
+	public void NetworkTest()
+	{
+		System.out.println("Will someday learn security testing");
+	}
+
+}
